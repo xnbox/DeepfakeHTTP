12376a7d66e991c633ccfce9ce08c622f4a13c4e
xnbox <xnbox.team@outlook.com>
2021-08-25 12:23:27 +0300

    command line help typo fixed

--- src/org/tommy/main/CustomMain.java
+++ src/org/tommy/main/CustomMain.java
@@ -50,15 +50,13 @@ public class CustomMain {
 			sb.append("\n");
 			sb.append(" Usage:\n");
 			sb.append("\n");
-			sb.append(" java -jar dhttp.jar [options]\n");
+			sb.append(" java -jar df.jar [options]\n");
 			sb.append("\n");
 			sb.append(" Options:\n");
 			sb.append("  --help          print help message\n");
 			sb.append("  --port          port number, default: 8080\n");
 			sb.append("  --no-listen     disable listening on dump(s) changes\n");
 			sb.append("  --no-etag       disable ETag optimization\n");
-			sb.append("  --dump <file> … dump file(s)\n");
-			sb.append("  --hook <file> … hook script(s)\n");
 			System.out.println(sb);
 			System.exit(0);
 		}
