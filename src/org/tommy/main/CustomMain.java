4d875dbd7e78810b7f48461b773acb568e2d09eb
xnbox <xnbox.team@outlook.com>
2021-09-07 11:45:09 +0300

    new CLI flags

--- src/org/tommy/main/CustomMain.java
+++ src/org/tommy/main/CustomMain.java
@@ -36,13 +36,16 @@ import java.util.Map;
 import org.deepfake_http.common.FirstLineReq;
 import org.deepfake_http.common.ReqResp;
 import org.deepfake_http.common.utils.JacksonUtils;
+import org.deepfake_http.common.utils.OpenApiUtils;
 import org.deepfake_http.common.utils.ParseCommandLineUtils;
-import org.deepfake_http.common.utils.ParseDumpUtils;
 
 public class CustomMain {
-	private static final String ARGS_HELP_OPTION = "--help";
-	private static final String ARGS_INFO        = "--info";
-	private static final String ARGS_REQUESTS    = "--requests";
+	private static final String ARGS_HELP_OPTION    = "--help";
+	private static final String ARGS_PRINT_INFO     = "--print-info";
+	private static final String ARGS_PRINT_REQUESTS = "--print-requests";
+	private static final String ARGS_PRINT_OPENAPI  = "--print-openapi";
+	private static final String ARGS_FORMAT         = "--format";
+	private static final String ARGS_PRETTYPRINT    = "--prettyprint";
 
 	/**
 	 * Custom main method (called for emmbedded web apps)
@@ -50,19 +53,28 @@ public class CustomMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		boolean help     = false;
-		boolean info     = false;
-		boolean requests = false;
+		boolean help        = false;
+		boolean info        = false;
+		boolean requests    = false;
+		boolean openapi     = false;
+		boolean prettyprint = false;
+		String  format      = "json";
+
 		for (int i = 0; i < args.length; i++)
 			if (args[i].equals(ARGS_HELP_OPTION)) {
 				help = true;
 				break;
-			} else if (args[i].equals(ARGS_INFO)) {
+			} else if (args[i].equals(ARGS_PRINT_INFO))
 				info = true;
-				break;
-			} else if (args[i].equals(ARGS_REQUESTS)) {
+			else if (args[i].equals(ARGS_PRINT_REQUESTS))
 				requests = true;
-				break;
+			else if (args[i].equals(ARGS_PRINT_OPENAPI))
+				openapi = true;
+			else if (args[i].equals(ARGS_PRETTYPRINT))
+				prettyprint = true;
+			else if (args[i].equals(ARGS_FORMAT)) {
+				if (i < args.length - 1)
+					format = args[++i].toLowerCase(Locale.ENGLISH);
 			}
 
 		if (help) {
@@ -75,27 +87,32 @@ public class CustomMain {
 			sb.append(" java -jar df.jar [options] [dump1.txt] [dump2.txt] ...\n");
 			sb.append("\n");
 			sb.append(" Options:\n");
-			sb.append("          --help           print help message\n");
-			sb.append("          --info           print dump files statistics as JSON\n");
-			sb.append("          --requests       print dump requests as JSON\n");
-			sb.append("          --port           TCP port number, default: 8080\n");
-			sb.append("          --collect <file> append live request/response dumps to file\n");
-			sb.append("          --no-log         disable request/response console logging\n");
-			sb.append("          --no-etag        disable ETag optimization\n");
-			sb.append("          --no-watch       disable watch dump files for changes\n");
+			sb.append("   --help                print help message\n");
+			sb.append("   --print-info          print dump files statistics to stdout as JSON/YAML\n");
+			sb.append("   --print-requests      print dump requests to stdout as JSON/YAML\n");
+			sb.append("   --print-openapi       print OpenAPI specification to stdout as JSON/YAML\n");
+			sb.append("   --format <json|yaml>  output format for --print-* options, default: json\n");
+			sb.append("   --prettyprint         prettyprint for --print-* options\n");
+			sb.append("   --openapi-path <path> serve OpenAPI spec and client at specified context path\n");
+			sb.append("   --port                TCP port number, default: 8080\n");
+			sb.append("   --collect <file>      append live request/response dumps to file\n");
+			sb.append("   --no-log              disable request/response console logging\n");
+			sb.append("   --no-etag             disable ETag optimization\n");
+			sb.append("   --no-watch            disable watch dump files for changes\n");
 
 			System.out.println(sb);
 			System.exit(0);
 		} else if (info) {
 			List<String /* dump file */> dumps          = new ArrayList<>();
-			boolean[]                    noListenArr    = new boolean[1];
+			boolean[]                    noWatchArr     = new boolean[1];
 			boolean[]                    noEtagArr      = new boolean[1];
 			boolean[]                    noLogArr       = new boolean[1];
 			String[]                     collectFileArr = new String[1];
+			String[]                     openApiPathArr = new String[1];
 			try {
-				ParseCommandLineUtils.parseCommandLineArgs(null, args, dumps, noListenArr, noEtagArr, noLogArr, collectFileArr);
+				ParseCommandLineUtils.parseCommandLineArgs(null, args, dumps, noWatchArr, noEtagArr, noLogArr, collectFileArr, openApiPathArr);
 
-				String json = serializeInfoToJson(dumps);
+				String json = serializeInfoToJson(dumps, format, prettyprint);
 				System.out.println(json);
 			} catch (Throwable e) {
 				e.printStackTrace();
@@ -103,15 +120,36 @@ public class CustomMain {
 			System.exit(0);
 		} else if (requests) {
 			List<String /* dump file */ > dumps          = new ArrayList<>();
-			boolean[]                     noListenArr    = new boolean[1];
+			boolean[]                     noWatchArr     = new boolean[1];
 			boolean[]                     noEtagArr      = new boolean[1];
 			boolean[]                     noLogArr       = new boolean[1];
 			String[]                      collectFileArr = new String[1];
+			String[]                      openApiPathArr = new String[1];
+
 			try {
-				ParseCommandLineUtils.parseCommandLineArgs(null, args, dumps, noListenArr, noEtagArr, noLogArr, collectFileArr);
+				ParseCommandLineUtils.parseCommandLineArgs(null, args, dumps, noWatchArr, noEtagArr, noLogArr, collectFileArr, openApiPathArr);
 				List<ReqResp> allReqResps = ParseCommandLineUtils.getAllReqResp(null, dumps);
 
-				String json = serializeRequestsToJson(allReqResps);
+				String json = serializeRequestsToJson(allReqResps, format, prettyprint);
+				System.out.println(json);
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+			System.exit(0);
+		} else if (openapi) {
+			List<String /* dump file */ > dumps          = new ArrayList<>();
+			boolean[]                     noWatchArr     = new boolean[1];
+			boolean[]                     noEtagArr      = new boolean[1];
+			boolean[]                     noLogArr       = new boolean[1];
+			String[]                      collectFileArr = new String[1];
+			String[]                      openApiPathArr = new String[1];
+
+			try {
+				ParseCommandLineUtils.parseCommandLineArgs(null, args, dumps, noWatchArr, noEtagArr, noLogArr, collectFileArr, openApiPathArr);
+				List<ReqResp> allReqResps = ParseCommandLineUtils.getAllReqResp(null, dumps);
+
+				Map<String, Object> openApiMap = OpenApiUtils.createOpenApiMap(allReqResps);
+				String              json       = JacksonUtils.stringifyToJsonYaml(openApiMap, format, prettyprint);
 				System.out.println(json);
 			} catch (Throwable e) {
 				e.printStackTrace();
@@ -127,7 +165,7 @@ public class CustomMain {
 	 * @return
 	 * @throws Throwable
 	 */
-	private static String serializeInfoToJson(List<String> dumps) throws Throwable {
+	private static String serializeInfoToJson(List<String> dumps, String format, boolean prettyprint) throws Throwable {
 		List<Map<String, Object>> list = new ArrayList<>(dumps.size());
 		for (String dumpFile : dumps) {
 			List<ReqResp>       dumpReqResp = ParseCommandLineUtils.getDumpReqResp(dumpFile);
@@ -136,7 +174,7 @@ public class CustomMain {
 			map.put("requestCount", dumpReqResp.size());
 			list.add(map);
 		}
-		return JacksonUtils.stringifyToJson(list);
+		return JacksonUtils.stringifyToJsonYaml(list, format, prettyprint);
 	}
 
 	/**
@@ -146,17 +184,18 @@ public class CustomMain {
 	 * @return
 	 * @throws Exception
 	 */
-	private static String serializeRequestsToJson(List<ReqResp> allReqResps) throws Throwable {
+	private static String serializeRequestsToJson(List<ReqResp> allReqResps, String format, boolean prettyprint) throws Throwable {
 		List<Map<String, Object>> list = new ArrayList<>(allReqResps.size());
 		for (ReqResp reqResp : allReqResps) {
 			Map<String, Object> map          = new LinkedHashMap<>();
-			FirstLineReq        firstLineReq = ParseDumpUtils.parseFirstLineReq(reqResp.request.firstLine);
-			map.put("method", firstLineReq.method.toUpperCase(Locale.ENGLISH));
-			map.put("requestURI", firstLineReq.uri);
+			FirstLineReq        firstLineReq = new FirstLineReq(reqResp.request.firstLine);
+			map.put("method", firstLineReq.getMethod().toUpperCase(Locale.ENGLISH));
+			map.put("requestURI", firstLineReq.getUri());
 			map.put("dumpFile", reqResp.dumpFile);
 			map.put("dumpFileLineNumber", reqResp.request.lineNumber);
 			list.add(map);
 		}
-		return JacksonUtils.stringifyToJson(list);
+		return JacksonUtils.stringifyToJsonYaml(list, format, prettyprint);
 	}
+
 }
