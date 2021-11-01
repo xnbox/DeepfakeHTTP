a164844587e178a171d67373cfd1de6d9fb607a3
xnbox <xnbox.team@outlook.com>
2021-11-01 20:06:04 +0200

    petclinic example updated

--- PetClinic/context.js
+++ PetClinic/context.js
@@ -1,14 +1,14 @@
-function getOwners(request, response, data) {
+function getOwners(request, response, db) {
 	const last_name = request.parameters.lastName[0];
 	let filteredOwners = {};
-	for (let owner_id in data.owners) {
-		let owner = data.owners[owner_id];
+	for (let owner_id in db.owners.data) {
+		let owner = db.owners.data[owner_id];
 		let okOwner = last_name.length == 0 ? true : owner.last_name.startsWith(last_name);
 		if (okOwner) {
 			owner['petNames'] = '';
 			let first = true;
-			for (let pet_id in data.pets) {
-				let pet = data.pets[pet_id];
+			for (let pet_id in db.pets.data) {
+				let pet = db.pets.data[pet_id];
 				let okPet = pet.owner_id == owner_id;
 				if (okPet) {
 					if (first)
@@ -24,28 +24,28 @@ function getOwners(request, response, data) {
 	response.body = filteredOwners;
 }
 
-function getOwner(request, response, data) {
+function getOwner(request, response, db) {
 	const id = request.parameters.id[0];
-	const owner = data.owners[id];
+	const owner = db.owners.data[id];
 	if (owner == undefined)
 		response.status = 404;
 	else
 		response.body = owner;
 }
 
-function getPets(request, response, data) {
+function getPets(request, response, db) {
 	const owner_id = request.parameters.id[0];
-	const owner = data.owners[owner_id];
+	const owner = db.owners.data[owner_id];
 	if (owner == undefined)
 		response.status = 404;
 	else {
 		const pets = {};
-		for (let pet_id in data.pets) {
-			let pet = data.pets[pet_id];
+		for (let pet_id in db.pets.data) {
+			let pet = db.pets.data[pet_id];
 			let okPet = pet.owner_id == owner_id;
 			if (okPet) {
 				let type_id = pet.type_id;
-				let type = data.types[type_id];
+				let type = db.types.data[type_id];
 				pet['type_name'] = type.name;
 				pets[pet_id] = pet;
 			}
@@ -54,16 +54,16 @@ function getPets(request, response, data) {
 	}
 }
 
-function getPet(request, response, data) {
+function getPet(request, response, db) {
 	const pet_id = request.parameters.id[0];
-	const pet = data.pets[pet_id];
+	const pet = db.pets.data[pet_id];
 	if (pet == undefined)
 		response.status = 404;
 	else
 		response.body = pet;
 }
 
-function newPet(request, response, data) {
+function newPet(request, response, db) {
 	const pet = {
 		owner_id: request.parameters.id[0],
 		name: request.parameters.name[0],
@@ -71,15 +71,14 @@ function newPet(request, response, data) {
 		type_id: request.parameters.type_id[0]
 	};
 
-	data.pets_id = "" + (parseInt(data.pets_id) + 1);
-	data.pets[data.pets_id] = pet;
+	let id = insert(db.pets, pet);
 
-	response.body = { id: data.pets_id };
+	response.body = { id: id };
 }
 
-function updatePet(request, response, data) {
+function updatePet(request, response, db) {
 	const pet_id = request.parameters.id[0];
-	const pet = data.pets[pet_id];
+	const pet = db.pets.data[pet_id];
 	if (pet == undefined)
 		response.status = 404;
 	else {
@@ -90,19 +89,19 @@ function updatePet(request, response, data) {
 	}
 }
 
-function getTypes(request, response, data) {
-	response.body = data.types;
+function getTypes(request, response, db) {
+	response.body = db.types.data;
 }
 
-function getVisits(request, response, data) {
+function getVisits(request, response, db) {
 	const pet_id = request.parameters.id[0];
-	const pet = data.pets[pet_id];
+	const pet = db.pets.data[pet_id];
 	if (pet == undefined)
 		response.status = 404;
 	else {
 		const visits = {};
-		for (let visit_id in data.visits) {
-			let visit = data.visits[visit_id];
+		for (let visit_id in db.visits.data) {
+			let visit = db.visits.data[visit_id];
 			let okVisit = visit.pet_id == pet_id;
 			if (okVisit)
 				visits[visit_id] = visit;
@@ -111,38 +110,35 @@ function getVisits(request, response, data) {
 	}
 }
 
-function newVisit(request, response, data) {
+function newVisit(request, response, db) {
 	const visit = {
 		pet_id: request.parameters.id[0],
 		visit_date: request.parameters.visitDate[0],
 		description: request.parameters.description[0]
 	};
 
-	data.visits_id = "" + (parseInt(data.visits_id) + 1);
-	data.visits[data.visits_id] = visit;
+	let id = insert(db.visits, visit);
 
-	response.body = { id: data.visits_id };
+	response.body = { id: id };
 }
 
 
-function newOwner(request, response, data) {
-	let owner = {};
+function newOwner(request, response, db) {
+	let owner = {
+		first_name: request.parameters.first_name[0],
+		last_name: request.parameters.last_name[0],
+		address: request.parameters.address[0],
+		city: request.parameters.city[0],
+		telephone: request.parameters.telephone[0]
+	}
+	let id = insert(db.owners, owner);
 
-	owner['first_name'] = request.parameters.first_name[0];
-	owner['last_name'] = request.parameters.last_name[0];
-	owner['address'] = request.parameters.address[0];
-	owner['city'] = request.parameters.city[0];
-	owner['telephone'] = request.parameters.telephone[0];
-
-	data.owners_id = "" + (parseInt(data.owners_id) + 1);
-	data.owners[data.owners_id] = owner;
-
-	response.body = { id: data.owners_id };
+	response.body = { id: id };
 }
 
-function updateOwner(request, response, data) {
+function updateOwner(request, response, db) {
 	const owner_id = request.parameters.id[0];
-	const owner = data.owners[owner_id];
+	const owner = db.owners.data[owner_id];
 	if (owner == undefined)
 		response.status = 404;
 	else {
@@ -154,27 +150,35 @@ function updateOwner(request, response, data) {
 	}
 }
 
-function getVets(request, response, data) {
+function getVets(request, response, db) {
 	const vets = {};
-	for (let vet_id in data.vets) {
-		let vet = data.vets[vet_id];
-	    let specialty_names = '';
-	    let first = true;
-	    for (let i = 0; i < data.vet_specialties.length; i++) {
-	    	let vet_specialty = data.vet_specialties[i];
-	    	if (vet_specialty.vet_id == vet_id) {
-	    		let specialty_id = vet_specialty.specialty_id
-	    		let specialty_name = data.specialties[specialty_id].name;
-	    		if (first)
-	    			first = false;
-	    		else
-	    			specialty_names += ', ';
-	    		specialty_names += specialty_name;
-	    	}
-	    }
-	    vet.specialty_names = specialty_names;
+	for (let vet_id in db.vets.data) {
+		let vet = db.vets.data[vet_id];
+		let specialty_names = '';
+		let first = true;
+		for (let i = 0; i < db.vet_specialties.length; i++) {
+			let vet_specialty = db.vet_specialties[i];
+			if (vet_specialty.vet_id == vet_id) {
+				let specialty_id = vet_specialty.specialty_id
+				let specialty_name = db.specialties.data[specialty_id].name;
+				if (first)
+					first = false;
+				else
+					specialty_names += ', ';
+				specialty_names += specialty_name;
+			}
+		}
+		vet.specialty_names = specialty_names;
 
 		vets[vet_id] = vet;
 	}
 	response.body = vets;
+}
+
+function insert(table, record) {
+	let id = table[""];
+	id = "" + (parseInt(id) + 1);
+	table[""] = id;
+	table.data[id] = record;
+	return id;
 }
